c4fedecab88144d240e0fe2671d91a9ee05f89a1
Brandon Bocklund <brandonbocklund@gmail.com>
2018-08-08 10:59:34 -0400

    Use an ordered dict in MCMC to ensure that parameter lnprob dict is ordered.

--- espei/mcmc.py
+++ espei/mcmc.py
@@ -7,6 +7,7 @@ Affine Invariance. Commun. Appl. Math. Comput. Sci. 5, 65-80 (2010).
 """
 
 import logging, time, sys
+from collections import OrderedDict
 
 import sympy
 import numpy as np
@@ -29,7 +30,7 @@ def lnprob(params, comps=None, dbf=None, phases=None, datasets=None,
     Returns the error from multiphase fitting as a log probability.
     """
     starttime = time.time()
-    parameters = {param_name: param for param_name, param in zip(symbols_to_fit, params)}
+    parameters = OrderedDict(zip(symbols_to_fit, params))
     try:
         multi_phase_error = calculate_zpf_error(dbf, comps, phases, datasets, phase_models,
                                      parameters=parameters, scheduler=scheduler,
